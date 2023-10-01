c288bbab97d981d4ed9b3bebba801be89120668c
narcolin <narcolin@calpoly.edu>
2023-10-01 13:53:13 -0700

    commit

--- src/MyApp.js
+++ src/MyApp.js
@@ -27,23 +27,4 @@ function MyApp() {
   )
 }
 
-// const characters = [
-//     {
-//       name: "Charlie",
-//       job: "Janitor",
-//     },
-//     {
-//       name: "Mac",
-//       job: "Bouncer",
-//     },
-//     {
-//       name: "Dee",
-//       job: "Aspring actress",
-//     },
-//     {
-//       name: "Dennis",
-//       job: "Bartender",
-//     },
-// ];
-
 export default MyApp;
--- src/Table.js
+++ src/Table.js
@@ -11,7 +11,7 @@ function TableHeader() {
       </thead>
     );
 }
-  
+
 function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
