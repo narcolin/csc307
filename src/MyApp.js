a74c2b7f27ddc47bd4b2fe911fbd1fe14d1426cb
narcolin <narcolin@calpoly.edu>
2023-10-08 14:25:20 -0700

    part6 first commits

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,21 +1,59 @@
-// src/MyApp.js
-
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table';
 import Form from './Form';
+import axios from 'axios';
 
 function MyApp() {
 	const [characters, setCharacters] = useState([]);  
 
-	function removeOneCharacter (index) {
-    const updated = characters.filter((character, i) => {
-        return i !== index
-    });
-    setCharacters(updated);
+  useEffect(() => {
+    fetchAll().then( result => {
+       if (result)
+          setCharacters(result);
+     });
+  }, [] );
+
+  async function makePostCall(person){
+    try {
+       const response = await axios.post('http://localhost:8000/users', person);
+       return response;
+    }
+    catch (error) {
+       console.log(error);
+       return false;
+    }
   }
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
+	async function removeOneCharacter (index) {
+    const userid = characters[index].id;
+    const response = await axios.delete('http://localhost:5001/users/'+userid); 
+    if(response && response.status === 204){
+      console.log('Deleted.');
+   }
+    const updated = characters.filter((character, i) => {
+        return i !== index
+      });
+      setCharacters(updated);
+    } 
+
+  function updateList(person) { 
+    makePostCall(person).then( result => {
+      if (result && result.status === 201) {
+        person = result.data;
+        setCharacters([...characters, person] );
+      }});
+    }
+
+  async function fetchAll(){
+    try {
+       const response = await axios.get('http://localhost:8000/users');
+       return response.data.users_list;     
+    }
+    catch (error){
+       //We're not handling errors. Just logging into the console.
+       console.log(error); 
+       return false;         
+    }
   }
 
   return (
